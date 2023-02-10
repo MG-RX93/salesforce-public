3092b7ca3fb5dbe0e50dc0abf0a0e596346001d0
MG-RX93 <jesseroshan.kanaparthi@meraki.net>
2023-02-10 15:35:39 -0800

    #4: Automating git setup

--- scripts/bash/prereqSetup.sh
+++ scripts/bash/prereqSetup.sh
@@ -17,3 +17,18 @@ fi
 echo
 echo "-------------------------------------------------------------------------------------------------------------------------"
 
+echo
+echo "$(tput setaf 3)Running git Checks"
+tput setaf 6;
+#checking to see if salesforce sfdx CLI exists and if not, install it.
+git --version
+if [[ $? != 0 ]] ; then
+    echo
+    echo "$(tput setaf 2)Installing git..."
+    brew install git
+else
+    echo
+    echo "$(tput setaf 2)git's already installed dummy!!!..."
+fi
+echo
+echo "-------------------------------------------------------------------------------------------------------------------------"
