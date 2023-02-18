c773689c0f6df371e81c96365b2f133e2c228315
MG-RX93 <mgrx.93v@gmail.com>
2023-02-17 16:01:53 -0800

    #4: Adding java checks - homebrew

---
+++ scripts/bash/DX Setup/java.sh
@@ -0,0 +1,21 @@
+function runJavaChecks() {
+  echo "$(tput setaf 3)-----------------------------------"
+  echo "$(tput setaf 3)|                                 |"
+  echo "$(tput setaf 3)|           Java Checks           |"
+  echo "$(tput setaf 3)|                                 |"
+  echo "$(tput setaf 3)-----------------------------------"
+  tput setaf 6;
+
+  java --version
+  if [[ $? != 0 ]] ; then
+      echo
+      echo "$(tput setaf 2)Installing java..."
+      read -p "$(tput setaf 3)Enter java package name: " pkgName
+      brew cask install "$pkgName"
+  else
+      echo
+      echo "$(tput setaf 2)Java's already installed..."
+  fi
+  echo
+  tput setaf 7;
+}
