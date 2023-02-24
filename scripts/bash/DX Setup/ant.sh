687007b17e529bdf0eb92fa21ce8cc1da3085725
MG-RX93 <mgrx.93v@gmail.com>
2023-02-23 16:04:58 -0800

    Formatting changes

--- scripts/bash/DX Setup/ant.sh
+++ scripts/bash/DX Setup/ant.sh
@@ -4,18 +4,18 @@ function runAntChecks() {
   echo "$(tput setaf 3)|           Ant Checks            |"
   echo "$(tput setaf 3)|                                 |"
   echo "$(tput setaf 3)-----------------------------------"
-  tput setaf 6;
+  tput setaf 6
 
   ant -version
-  if [[ $? != 0 ]] ; then
-      echo
-      echo "$(tput setaf 2)Installing ant..."
-      read -p "$(tput setaf 3)Enter ant package name: " pkgName
-      brew install "$pkgName"
+  if [[ $? != 0 ]]; then
+    echo
+    echo "$(tput setaf 2)Installing ant..."
+    read -p "$(tput setaf 3)Enter ant package name: " pkgName
+    brew install "$pkgName"
   else
-      echo
-      echo "$(tput setaf 2)Ant's already installed..."
+    echo
+    echo "$(tput setaf 2)Ant's already installed..."
   fi
   echo
-  tput setaf 7;
+  tput setaf 7
 }
--- scripts/bash/DX Setup/java.sh
+++ scripts/bash/DX Setup/java.sh
@@ -4,18 +4,18 @@ function runJavaChecks() {
   echo "$(tput setaf 3)|           Java Checks           |"
   echo "$(tput setaf 3)|                                 |"
   echo "$(tput setaf 3)-----------------------------------"
-  tput setaf 6;
+  tput setaf 6
 
   java --version
-  if [[ $? != 0 ]] ; then
-      echo
-      echo "$(tput setaf 2)Installing java..."
-      read -p "$(tput setaf 3)Enter java package name: " pkgName
-      brew install "$pkgName"
+  if [[ $? != 0 ]]; then
+    echo
+    echo "$(tput setaf 2)Installing java..."
+    read -p "$(tput setaf 3)Enter java package name: " pkgName
+    brew install "$pkgName"
   else
-      echo
-      echo "$(tput setaf 2)Java's already installed..."
+    echo
+    echo "$(tput setaf 2)Java's already installed..."
   fi
   echo
-  tput setaf 7;
+  tput setaf 7
 }
