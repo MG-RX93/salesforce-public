7c75e1db4fb071c83fe174412bc071e6b9b827a2
MG-RX93 <mgrx.93v@gmail.com>
2023-06-05 16:37:37 -0700

    Split grplogs file to stream & parse files

--- scripts/bash/parselogs.sh
+++ scripts/bash/parselogs.sh
@@ -1,17 +1,4 @@
 #!/bin/bash
-
-read -p "Enter org name(avoid spaces): " orgName
-read -p "Enter file name(avoid spaces): " fileName
-# Stream logs
-sfdx apex tail log -c --loglevel=debug -d=Debug -o "$orgName" | tee ~/Desktop/PTLogsDump/"$fileName".log
-
-read -p "Enter Test Class name(avoid spaces): " testClassName
-# Run unit tests
-sfdx force:apex:test:run -n "$testClassName" -o "$orgName" -r json
-
-
-
-# ===================================================
 # bulk parse logs 
 
 # 
