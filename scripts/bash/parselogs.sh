eb2806707a8fbccef3b4e1c9c2e914ae3e1f7c91
MG-RX93 <mgrx.93v@gmail.com>
2023-06-14 17:06:55 -0700

    Function to calculate transaction runtime

--- scripts/bash/parselogs.sh
+++ scripts/bash/parselogs.sh
@@ -2,19 +2,46 @@
 # bulk parse logs 
 
 # 
-cd ~/Desktop/PTLogs
+# time="00:20:40:300"
+# seconds=$(echo $time | awk -F: '{ print ($1 * 3600) + ($2 * 60) + $3 }')
+# echo $seconds
+
+function runTime() { 
+    runTime=$(echo "$1,$2,$3" | awk -F, '{ print (($1 - $2) * $3)}')
+    echo $runTime
+}
+
+pushd ~/Desktop/PTLogs
 
 # https://linuxhint.com/bash-for-loop-examples/
+
+# for f in *.log
+# do
+#     # Print the string value
+#     # https://www.linode.com/docs/guides/how-to-grep-for-text-in-files/
+#     echo $f
+#     cat $f | sed '/^[[:space:]]*$/d' | tee ./nonewline/$f
+# done
+
+declare -A runTime_log_array
+
+pushd ~/Desktop/PTLogs/nonewline
 for f in *.log
 do
     # Print the string value
     # https://www.linode.com/docs/guides/how-to-grep-for-text-in-files/
     echo $f
-    cat $f | grep -E "USER_DEBUG|FATAL_ERROR|EXCEPTION_THROWN|_ERROR" | tee ~/Desktop/PTLogs/grep/$f
+    startTime=$(sed -n '2p' $f | awk -F: '{ print ($1 * 3600) + ($2 * 60) + $3 + ($4 / 1000)}')
+    endTime=$(sed -n '$p' $f | awk -F: '{ print ($1 * 3600) + ($2 * 60) + $3 + ($4 / 1000)}')
+    # cpuTime=$(echo "$endTime,$startTime,1000" | awk -F, '{ print (($1 - $2) * $3)}')
+    runTime=$(runTime "$endTime,$startTime,1000")
+    echo "$runTime"
+    runTime_log_array["${f%.*}"]="$runTime"
+    cat $f | grep -E "USER_DEBUG|FATAL_ERROR|EXCEPTION_THROWN|_ERROR" | tee .././grep/$f
 done
 
 # 
-cd ~/Desktop/PTLogs/grep/
+pushd ~/Desktop/PTLogs/grep/
 
 # 
 for f in *.log
@@ -30,3 +57,4 @@ do
 
     # O/p of grep must add commas to the different values it prints or use jq to add commas
 done
+
