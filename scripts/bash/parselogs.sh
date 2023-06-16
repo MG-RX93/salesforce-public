45a85d5328a15ff08f68632332f4371a22b16570
MG-RX93 <mgrx.93v@gmail.com>
2023-06-16 10:33:34 -0700

    Adding comments

--- scripts/bash/parselogs.sh
+++ scripts/bash/parselogs.sh
@@ -1,29 +1,33 @@
 #!/bin/bash
 
+# Process timestamps & get runtime
 function runTime() { 
     runTime=$(echo "$1,$2,$3" | awk -F, '{ print (($1 - $2) * $3)}')
     echo $runTime
 }
 
+# Change directory
 pushd ~/Desktop/PTLogs
 
-# https://linuxhint.com/bash-for-loop-examples/
-
+# Loop through log files & remove empty lines from all files
 for f in *.log
 do
-    # Print the string value
-    # https://www.linode.com/docs/guides/how-to-grep-for-text-in-files/
-    echo $f
-    cat $f | sed '/^[[:space:]]*$/d' | tee ./nonewline/$f
+    # echo $f
+    cat $f | sed '/^[[:space:]]*$/d' | tee ./nonewline/$f #remove empty lines
 done
 
-declare -A runTime_log_array
+# declare associative array
+declare -A runTime_log_array 
 
+# Change directory
 pushd ~/Desktop/PTLogs/nonewline
+# Loop through processed log files.
+# Grab startTime & endTime to get runTime.
+# Assign runTime to the associative array var.
+# Grab only those lines with USER_DEBUG & create a processed log file.
 for f in *.log
 do
     # Print the string value
-    # https://www.linode.com/docs/guides/how-to-grep-for-text-in-files/
     echo $f
     startTime=$(sed -n '2p' $f | awk -F: '{ print ($1 * 3600) + ($2 * 60) + $3 + ($4 / 1000)}')
     endTime=$(sed -n '$p' $f | awk -F: '{ print ($1 * 3600) + ($2 * 60) + $3 + ($4 / 1000)}')
@@ -33,21 +37,23 @@ do
     cat $f | grep -E "USER_DEBUG|FATAL_ERROR|EXCEPTION_THROWN|_ERROR" | tee .././grep/$f
 done
 
+# Change directory
 pushd ~/Desktop/PTLogs/grep/
-
+# Loop through processed log files.
+# Use grep to only pull json output.
+# Convert grepop to json & create a processed json file.
 for f in *.log
 do
     echo $f
-    # https://stackoverflow.com/questions/4168371/how-can-i-remove-all-text-after-a-character-in-bash
-    # https://tecadmin.net/linux-jq-command/
-    # https://stackoverflow.com/questions/51183073/extract-json-data-from-log-file
     echo "${runTime_log_array["${f%.*}"]}" # Value for the passed key
     grepop=$(cat $f | grep -o '{".*}')
     echo $grepop | jq -s . | tee ~/Desktop/PTLogs/json/first/"${f%.log}".json
 done
 
+# Change directory
 pushd ~/Desktop/PTLogs/json/first/
-
+# Loop through processed json files.
+# Append trigger type to file name to make it unique & create new file for each trigger type.
 for f in *.json
 do
     jq -c '.[]' $f | while read i; do
@@ -65,7 +71,10 @@ do
     done
 done
 
+# Change directory
 pushd ~/Desktop/PTLogs/json/second/
+# Loop through processed json files.
+# Add runtime as item to each json object.
 for f in *.json
 do
     runTimeValue=$(echo "${runTime_log_array["${f%.*}"]}")
