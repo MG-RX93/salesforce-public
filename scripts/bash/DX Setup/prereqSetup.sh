1cf248f36df091e3f1663fd901be9c3d8707412a
MG-RX93 <mgrx.93v@gmail.com>
2024-03-04 15:54:11 -0800

    Remove unwanted files

--- scripts/bash/DX Setup/prereqSetup.sh
+++
@@ -1,8 +0,0 @@
-# Run Homebrew checks
-. scripts/bash/DX%20Setup/homebrew.sh
-runHomeBrewChecks
-
-# Run Git checks
-. scripts/bash/DX%20Setup/git.sh
-runGitChecks
-
--- scripts/bash/Fish/omf_install.sh
+++
@@ -1,2 +0,0 @@
-# Run the following to get started with the defaault setup
-curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
