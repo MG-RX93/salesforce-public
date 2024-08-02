b3356d7a24f2d44713e6fe859d310f0ba3ec3a4e
MG-RX93 <mgrx.93v@gmail.com>
2024-08-02 16:31:04 -0700

    Update dynamic var in path

--- scripts/sf/validation_results.sh
+++ scripts/sf/validation_results.sh
@@ -3,4 +3,4 @@ set -a  # Automatically export all vars
 . ./.env
 set +a  # Disable Automatic export
 
-sf project deploy report --job-id $JOB_ID --json | tee docs/$DIR_NAME/validation_results/$JOB_ID.json
+sf project deploy report --job-id $JOB_ID --json | tee docs/$PACKAGE_NAME/validation_results/$JOB_ID.json
